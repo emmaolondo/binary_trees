0795c85e6c0fbbf70cc2dcb92c8bfe47e4ab9519
root <emmaolondo448@gmail.com>
2023-11-03 12:51:57 -0700

    insert left node correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 	left_height = binary_tree_height(tree->left) + 1;
-	right_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
 	if (left_height > right_height)
 		return (left_height);
 	else
