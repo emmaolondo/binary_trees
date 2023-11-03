b94c2cb91104ab9a8fdbddf07d04ee11edc8ecfb
root <emmaolondo448@gmail.com>
2023-11-03 12:48:26 -0700

    insert left node correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,9 +10,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *temp, *newNode;
 
+	if (parent == NULL)
+		return (NULL);
+
 	newNode = malloc(sizeof(binary_tree_t));
 
-	if (newNode == NULL || parent == NULL)
+	if (newNode == NULL)
 		return (NULL);
 
 	newNode->n = value;
